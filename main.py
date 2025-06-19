3738a52aa435e148ae57ecbafa45a7805352b34a
Joaco2025 <joaquinalf177@gmail.com>
2025-06-18 20:17:22 -0700

    Primer Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#main.py
+import pygame
+import sys
+
+# Inicializar Pygame
+pygame.init()
+
+#Configuración de pantalla (el tamaño que tendrá la pantalla en pixeles)
+pygame.display.set_mode((800, 600))
+#El nombre que tendrá la pantalla iniciada
+pygame.display.set_caption("Amaya Joto")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
