080f9eb92df08d9aeb64df672c5a985ceabad90a
Joaco2025 <joaquinalf177@gmail.com>
2025-06-18 20:31:41 -0700

    pantalla simple en main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import sys
 
 # Inicializar Pygame
 pygame.init()
-
 #Configuración de pantalla (el tamaño que tendrá la pantalla en pixeles)
 pygame.display.set_mode((800, 600))
 #El nombre que tendrá la pantalla iniciada
 pygame.display.set_caption("Amaya Joto")
-
+#Codigo extremadamente simple para abrir una pantalla, y que se cierre cuanto presiones exit
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
